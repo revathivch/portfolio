aa535c12538f4b40c6b04133a52a3be9349fc04d
Revathi Vipinachandran <revathivch@gmail.com>
2024-01-04 14:38:07 -0500

    Added constant.js to store the data

---
+++ src/data/constants.js
@@ -0,0 +1,256 @@
+export const Biodata = {
+    name: "Revathi Vipinachandran",
+    roles: [
+        "Software Engineer",
+    ],
+    description: "I find my passion in unraveling complex problems and immersing myself in the world of technology, where I'm committed to turning innovative ideas into real-world, impactful solutions.",
+    github_link: "https://github.com/revathivch",
+    resume_link: "",
+    linkedin_link: "https://www.linkedin.com/in/revathi-vipinachandran-246800183/",
+    twitter_link: "",
+    instagram_link: "",
+    facebook_link: "",
+}
+
+export const skills = [
+    {
+        title: "Programming Languages",
+        skills: [
+            {
+                name: "C",
+                image: "",
+            },
+            {
+                name: "C++",
+                image: "",
+            },
+            {
+                name: "Java",
+                image: "",
+            },
+            {
+                name: "Python",
+                image: "",
+            },
+            {
+                name: "JavaScript",
+                image: "",
+            },
+            {
+                name: "TypeScript",
+                image: "",
+            },
+            {
+                name: "C#",
+                image: "",
+            },
+            {
+                name: "SQL",
+                image: "",
+            },
+
+        ],
+    },
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "React Js",
+                image: "",
+            },
+            {
+                name: "Figma",
+                image: "",
+            },
+            {
+                name: "HTML",
+                image: "",
+            },
+            {
+                name: "CSS",
+                image: "",
+            },
+            {
+                name: "Bootstrap",
+                image: "",
+            },
+            {
+                name: "Material UI",
+                image: "",
+            },
+            {
+                name: "PrimeReact",
+                image: "",
+            },
+            {
+                name: "Flutter",
+                image: ""
+            }
+
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Node Js",
+                image: "",
+            },
+            {
+                name: "Express Js",
+                image: "",
+            },
+            {
+                name: "MySQL",
+                image: "",
+            },
+            {
+                name: "PostreSQL",
+                image: "",
+            },
+            {
+                name: "MongoDB",
+                image: "",
+            },
+            {
+                name: "Firebase",
+                image: "",
+            },
+            {
+                name: "Firestore",
+                image: "",
+            },
+            {
+                name: "Postman",
+                image: "",
+            },
+        ],
+    },
+    {
+        title: "Cloud and Deployment",
+        skills: [
+            {
+                name: "Microsoft Azure",
+                image: "",
+            },
+            {
+                name: "Google Cloud Computing",
+                image: "",
+            },
+            {
+                name: "Docker",
+                image: "",
+            },
+            {
+                name: "Kubernetes",
+                image: "",
+            },
+            {
+                name: "GitHub",
+                image: "",
+            },
+            {
+                name: "Bitbucket",
+                image: "",
+            },
+        ],
+    },
+]
+
+export const work_experience = [
+    {
+        title: "Software Engineer",
+        image: "",
+        company: "Honeywell Inc.",
+        date: "August 2021 - August 2022",
+        responsibilities: "",
+        skills: [],
+    },
+    {
+        title: "Software Engineer Intern",
+        image: "",
+        company: "Honeywell Inc.",
+        date: "February 2021 - August 2021",
+        responsibilities: "",
+        skills: [],
+    },
+    {
+        title: "Business Analyst Intern",
+        image: "",
+        company: "Dell Technologies",
+        date: "June 2020 - September 2020",
+        responsibilities: "",
+        skills: [],
+    },
+]
+
+export const projects = [
+    {
+        project_name: "Kaleidoscope Collaborative",
+        date: "September 2023 - December 2023",
+        image: "",
+        description: "",
+        skills: [],
+        category: "web application",
+        github: "https://github.com/BU-Spark/se-kaleidoscope-collaborative",
+    },
+    {
+        project_name: "Java Bank Application",
+        date: "September 2022 - December 2022",
+        image: "",
+        description: "",
+        skills: [],
+        category: "java application",
+        github: "https://github.com/revathivch/BankApplication",
+        member: [
+            {
+              name: "",
+              image: "",
+            },
+            {
+              name: "",
+              image: "",
+            },
+          ],
+    },
+    {
+        project_name: "Small Object Detection for Spotting Birds",
+        date: "January 2023 - May 2023",
+        image: "",
+        description: "",
+        skills: [],
+        category: "Machine Learning",
+        github: "https://github.com/revathivch/BankApplication",
+        member: [
+            {
+              name: "",
+              image: "",
+            },
+            {
+              name: "",
+              image: "",
+            },
+          ],
+    },
+    {
+        project_name: "Foreign Exchange Rate Prediction",
+        date: "January 2019 - May 2019",
+        image: "",
+        description: "",
+        skills: [],
+        category: "Machine Learning",
+        github: "",
+        member: [
+            {
+              name: "",
+              image: "",
+            },
+            {
+              name: "",
+              image: "",
+            },
+          ],
+    },
+]
+
+
